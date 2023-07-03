174ff223c2f5f3975786b92047f5feea15ac1afb
Basileus <Basileus@DESKTOP-M14F8KM>
2023-07-03 20:35:54 +0300

    Добавьте файлы проекта.

---
+++ Swimming.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <thread>
+#include <mutex>
+
+class Swimmer
+{
+    std::string name;
+    int speed;
+
+public:
+    bool finish;
+    int distance = 100;
+
+    void setName(std::string inName)
+    {
+        name = inName;
+    }
+
+    void setSpeed(int inSpeed)
+    {
+        speed = inSpeed;
+    }
+
+    std::string getName()
+    {
+        return name;
+    }
+
+    int getSpeed()
+    {
+        return speed;
+    }
+
+    void swim()
+    {
+
+    }
+
+    Swimmer(std::string inName, int inSpeed)
+    {
+        setName(inName);
+        setSpeed(inSpeed);
+        finish = false;
+    }
+};
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
