a237625036d7c8b2ab010655e3d52fc8cb9814e0
Basileus <Basileus@DESKTOP-M14F8KM>
2023-07-04 22:00:36 +0300

    function

--- Swimming.cpp
+++ Swimming.cpp
@@ -1,6 +1,11 @@
 ﻿#include <iostream>
 #include <thread>
 #include <mutex>
+#include <vector>
+#include <ctime>
+
+std::vector<Swimmer*> competition;
+std::mutex access;
 
 class Swimmer
 {
@@ -9,7 +14,8 @@ class Swimmer
 
 public:
     bool finish;
-    int distance = 100;
+    //std::time_t timer;
+    int distance;
 
     void setName(std::string inName)
     {
@@ -33,7 +39,10 @@ public:
 
     void swim()
     {
-
+        while (distance > 0)
+        {
+            distance += speed;
+        }
     }
 
     Swimmer(std::string inName, int inSpeed)
@@ -41,10 +50,24 @@ public:
         setName(inName);
         setSpeed(inSpeed);
         finish = false;
+        distance = 0;
+        //timer = std::time(nullptr);
     }
 };
 
 int main()
 {
+    std::string tempName;
+    int tempSpeed = 0;
+
     std::cout << "Hello World!\n";
+    for (int i = 0; i < 10; i++)
+    {
+        std::cout << i+1 << ".\nEnter swimmer's name: ";
+        std::cin >> tempName;
+        std::cout << "Enter swimmer's speed: ";
+        std::cin >> tempSpeed;
+        Swimmer* swimmer = new Swimmer(tempName, tempSpeed);
+        competition.push_back(swimmer);
+    }
 }
