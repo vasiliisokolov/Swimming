ac0a1d74595498a52603995f897d727ceb03d97c
Basileus <Basileus@DESKTOP-M14F8KM>
2023-07-07 18:49:21 +0300

    Done!

--- Swimming.cpp
+++ Swimming.cpp
@@ -36,7 +36,20 @@ public:
         return speed;
     }
 
-    
+    void swim()
+    {
+        {
+            while (distance < 100)
+            {
+                std::this_thread::sleep_for(std::chrono::seconds(1));
+                distance += getSpeed();
+                std::cout << getName() << " swam " << distance << " meters!" << std::endl;
+            }
+            output_access.lock();
+            result.push_back(getName());
+            output_access.unlock();
+        }
+    }
 
     Swimmer(std::string inName, int inSpeed)
     {
@@ -48,20 +61,7 @@ public:
 };
 
 
-void swim(Swimmer* swimmer)
-{
-    {
-        while (swimmer->distance < 100)
-        {
-            std::this_thread::sleep_for(std::chrono::seconds(1));
-            swimmer->distance += swimmer->getSpeed();
-            std::cout << swimmer->getName() << " swam " << swimmer->distance << " meters!" << std::endl;
-        }
-        output_access.lock();
-        result.push_back(swimmer->getName());
-        output_access.unlock();
-    }
-}
+
 
 int main()
 {
@@ -81,37 +81,16 @@ int main()
     }
     std::cout << "Start!" << std::endl;   
     
-    output_access.lock();
-    std::thread swimmer0(swim, swimmers[0]);
-    output_access.unlock();
-
-    output_access.lock();
-    std::thread swimmer1(swim, swimmers[1]);
-    output_access.unlock();
-
-    output_access.lock();
-    std::thread swimmer2(swim, swimmers[2]);
-    output_access.unlock();
-
-    output_access.lock();
-    std::thread swimmer3(swim, swimmers[3]);
-    output_access.unlock();
-
-    output_access.lock();
-    std::thread swimmer4(swim, swimmers[4]);
-    output_access.unlock();
-
-    output_access.lock();
-    std::thread swimmer5(swim, swimmers[5]);
-    output_access.unlock();
+    for (int i = 0; i < 6; i++)
+    {
+        tracks.emplace_back(&Swimmer::swim, swimmers[i]);
+    }
     
-    swimmer0.join();
-    swimmer1.join();
-    swimmer2.join();
-    swimmer3.join();
-    swimmer4.join();
-    swimmer5.join();
-    
+    for (int i = 0; i < 6; i++)
+    {
+        tracks[i].join();
+    }
+        
     std::cout << "The competition is ower! Results: " << std::endl;
     output_access.lock();
     for (int i = 0; i < 6; i++)
